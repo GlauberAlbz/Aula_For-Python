dbd9ea5b1268d97c2844112704ca181aadd6f05c
GlauberAlbz <glauberalmeidab@gmail.com>
2025-09-18 22:05:11 -0300

    Exercicio 01 finalizado

--- ex01.py
+++ ex01.py
@@ -6,3 +6,15 @@
     - Faça um programa que mostre na tela uma contagem regressiva para o estouro de fogos de artifício,
       indo de 10 até 0, com uma pausa de 1 segundo entre eles.
 '''
+
+import os
+import time 
+
+os.system('cls')
+
+print("Segue a contagem regressiva dos fogos de artifício:")
+
+for c in range(10, 0, -1):
+    print(c)
+    time.sleep(1)
+print("FELIZ ANO NOVO!!!🎆🎆🎆")
