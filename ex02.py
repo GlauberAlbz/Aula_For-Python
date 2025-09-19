c3e7f1917b532a025896a7b61796836b2b14816f
GlauberAlbz <glauberalmeidab@gmail.com>
2025-09-18 22:05:23 -0300

    Exercicio 02 finalizado

--- ex02.py
+++ ex02.py
@@ -5,3 +5,8 @@
     Exercício 02
     - Crie um programa que mostre na tela todos os número pares que estão no intervalo entre 1 e 50.
 '''
+
+print("Segue todos os número pares ente 1 e 50:")
+
+for c in range(2,51, 2):
+    print(c)
