6b6855653cb3684813d1ee1539a461f93fd63dd6
Pozenatos <luishenrique.n.c.pozenato@gmail.com>
2025-09-21 22:04:16 -0300

    ex05.py
    EX05 Finalizado

--- ex05.py
+++ ex05.py
@@ -6,3 +6,19 @@
     - Desenvolva um programa que leia 6 números interios e mostre a soma apenas daqueles que forem pares.
       Se o valor digitado for impar, desconsidere-o.
 '''
+
+import os
+
+os.system('cls')
+
+soma = 0
+
+for c in range(6):  
+    num = int(input(f"Defina o valor do número {c+1}: "))
+    if num % 2 == 0:  
+        soma += num
+    else:
+        print(f'O número {num} é impar, logo desconsideraremos esse valor da soma.')
+print('\n')
+print(f'O resultado da conta apenas somando os pares deu: {soma}!\n')
+print('Obrigado por usar este programa!')
